f03c9d9adc6cee056df90267c30ccfe9ec7ea0aa
pnavgire998@gmail.com <pnavgire998@gmail.com>
2023-01-03 04:28:52 +0530

    fix: correcting the host env variable

--- src/endpoints/UsersEndpoints.js
+++ src/endpoints/UsersEndpoints.js
@@ -1,4 +1,4 @@
-userServiceBaseUrl = process.env.SKILLS_SERVICE_URL || 'http://locahost:8080'
+userServiceBaseUrl = process.env.USER_SKILLS_SERVICE_URL || 'http://locahost:8080'
 
 const updateExpertise = userId => `/api/users/${userId}/expertise`
 
